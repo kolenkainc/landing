bee6ea7cbfd0f533f7817fb8355f182e5ff5d5fd
Dmitry Romashov <framebassman@gmail.com>
2020-05-15 18:29:25 +0300

    add Academy font

--- src/App/index.svelte
+++ src/App/index.svelte
@@ -1,12 +1,10 @@
 <script>
     import Layout from '@Components/Layout';
-    import ImageExample from '@Components/ImageExample';
     import Title from '@Components/Title';
     import Cards from '@Components/Cards';
 </script>
 
 <style src="./style.scss">
-
 </style>
 
 <Layout>
